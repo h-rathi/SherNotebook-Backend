f0d9384ab823533bbeb3f16669acef9ad1710d28
h-rathi <thehimanshurathi@gmail.com>
2025-02-07 22:07:05 +0530

    mongodb atlas link added instead of local server

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 const run= async ()=>{
-     await mongoose.connect('mongodb://127.0.0.1:27017/')
+     await mongoose.connect('mongodb+srv://thehimanshurathi:thehimanshurathi@cluster0.skfs9.mongodb.net/')
      console.log("connected")
 }
 module.exports=run;
